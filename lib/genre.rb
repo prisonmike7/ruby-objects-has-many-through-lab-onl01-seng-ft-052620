99758b180b1b969ec7bae63dcb82f950a70b23f2
Michael Harris <mwharris7@gmail.com>
2020-06-13 13:07:55 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -14,7 +14,13 @@ class Genre
   end
 
   def songs
-
+    new_array = []
+    Song.all.each do|song|
+      if song.genre == self
+        new_array << song
+      end
+    end
+    new_array
   end
 
   def artists
