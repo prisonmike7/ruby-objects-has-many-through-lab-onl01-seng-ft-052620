3404bac140fb8f9bda2c213a46b9fa9794cbc552
Michael Harris <mwharris7@gmail.com>
2020-06-13 13:09:50 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,28 @@
+class Doctor
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(name, genre)
+
+  end
+
+  def appointments
+
+  end
+
+  def patients
+
+  end
+
+end
