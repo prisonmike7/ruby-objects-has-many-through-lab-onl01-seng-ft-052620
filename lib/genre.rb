6fd8b1bf2df895eaaad864e667c3f3d653c0aa9f
Michael Harris <mwharris7@gmail.com>
2020-06-13 12:55:54 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -13,16 +13,11 @@ class Genre
     @@all
   end
 
-  def new_song(name)
-    song_new = Song.new(name)
-    song_new.artist = self
-  end
-
   def songs
 
   end
 
-  def genres
+  def artists
 
   end
 
